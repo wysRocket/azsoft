0d36c12e47028445d067d0bacf6614361b6bbd97
Vladimir Kvirenko <vkvirenko@icloud.com>
2020-06-25 12:55:44 +0300

    triggers

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,19 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { useForm } from "react-hook-form";
 import { CacheContext } from "./../context/cacheContext";
 
 export const CacheForm = () => {
   const { set, formValues, resetInputs } = useContext(CacheContext);
-  const { register, handleSubmit, errors } = useForm();
-
+  const { register, handleSubmit, triggerValidation } = useForm();
+  const [triggers, setTriggers] = useState([]);
   const onSubmit = (data, e) => {
     set(data);
     e.target.reset();
     resetInputs();
   };
   console.log("formValues:", formValues);
-  console.log("form errors:", errors);
+  console.log("triggers:", triggers);
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>Key :</label>
@@ -20,6 +21,10 @@ export const CacheForm = () => {
         name="key"
         ref={register({ required: true })}
         defaultValue={formValues.key}
+        onChange={async () => {
+          const keyTrigger = await triggerValidation("key");
+          setTriggers([...triggers, keyTrigger]);
+        }}
       />
 
       <label> Value :</label>
@@ -27,8 +32,12 @@ export const CacheForm = () => {
         name="value"
         ref={register({ required: true })}
         defaultValue={formValues.value}
+        onChange={async () => {
+          const valueTrigger = await triggerValidation("value");
+          setTriggers([...triggers, valueTrigger]);
+        }}
       />
-      {errors.key || errors.value ? null : <input type="submit" />}
+      {triggers.find((t) => t === undefined) ? null : <input type="submit" />}
     </form>
   );
 };
